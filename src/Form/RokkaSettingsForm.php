7e93db99d718e20a02367fe92110a167341d1b04
Emanuele Panzeri <emanuele.panzeri@liip.ch>
2017-06-22 21:21:32 +0200

    Updated requirements from Rokka service

--- src/Form/RokkaSettingsForm.php
+++ src/Form/RokkaSettingsForm.php
@@ -10,80 +10,87 @@ use Drupal\Core\Form\FormStateInterface;
  */
 class RokkaSettingsForm extends ConfigFormBase
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormId()
+    {
+        return 'rokka_admin_settings_form';
+    }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getFormId()
-  {
-      return 'rokka_admin_settings_form';
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function getEditableConfigNames()
-  {
-      return ['rokka.settings'];
-  }
+    /**
+     * {@inheritdoc}
+     */
+    protected function getEditableConfigNames()
+    {
+        return ['rokka.settings'];
+    }
 
     public function buildForm(array $form, FormStateInterface $form_state)
     {
         $config = $this->config('rokka.settings');
 
         $form = array(
-      'is_enabled' => array(
-        '#title' => $this->t('Enable Rokka.io service'),
-        '#description' => $this->t('Enable or disable the Rokka.io integration'),
-        '#type' => 'checkbox',
-        '#default_value' => $config->get('is_enabled'),
-      ),
-      'credentials' => array(
-        '#type' => 'fieldset',
-        '#title' => $this->t('API Credentials'),
-        '#description' => $this->t('Enter your Rokka.io API credentials'),
-        '#collapsible' => false,
+            'is_enabled' => array(
+                '#title' => $this->t('Enable Rokka.io service'),
+                '#description' => $this->t('Enable or disable the Rokka.io integration'),
+                '#type' => 'checkbox',
+                '#default_value' => $config->get('is_enabled'),
+            ),
+            'credentials' => array(
+                '#type' => 'fieldset',
+                '#title' => $this->t('API Credentials'),
+                '#description' => $this->t('Enter your Rokka.io API credentials'),
+                '#collapsible' => false,
 
-        'rokka_api_key' => array(
-          '#title' => $this->t('API Key'),
-          '#description' => $this->t('The API Key credential provided by the Rokka.io service'),
-          '#type' => 'textfield',
-          '#required' => true,
-          '#default_value' => $config->get('rokka_api_key'),
-        ),
-      ),
-      'organization' => array(
-        '#type' => 'fieldset',
-        '#title' => $this->t('Organization Credentials'),
-        '#description' => $this->t('Enter the Organization at Rokka.io'),
-        '#collapsible' => false,
+                'api_key' => array(
+                    '#title' => $this->t('API Key'),
+                    '#description' => $this->t('The API Key credential provided by the Rokka.io service'),
+                    '#type' => 'textfield',
+                    '#required' => true,
+                    '#default_value' => $config->get('api_key'),
+                ),
+            ),
+            'organization' => array(
+                '#type' => 'fieldset',
+                '#title' => $this->t('Organization Credentials'),
+                '#description' => $this->t('Enter the Organization at Rokka.io'),
+                '#collapsible' => false,
 
-        'organization_name' => array(
-          '#title' => $this->t('Organization Name'),
-          '#description' => $this->t('The Organization Name given from the Rokka.io service'),
-          '#type' => 'textfield',
-          '#required' => true,
-          '#default_value' => $config->get('organization_name'),
-        )
-      ),
-    );
+                'organization_name' => array(
+                    '#title' => $this->t('Organization Name'),
+                    '#description' => $this->t('The Organization Name given from the Rokka.io service'),
+                    '#type' => 'textfield',
+                    '#required' => true,
+                    '#default_value' => $config->get('organization_name'),
+                ),
+            ),
+            'api_endpoint' => array(
+                '#title' => $this->t('API Endpoint'),
+                '#description' => $this->t('The API endpoint'),
+                '#type' => 'textfield',
+                '#required' => true,
+                '#default_value' => $config->get('api_endpoint'),
+            ),
+        );
 
         return parent::buildForm($form, $form_state);
     }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state)
-  {
-      $values = $form_state->getValues();
-      $config = $this->config('rokka.settings');
+    /**
+     * {@inheritdoc}
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        $values = $form_state->getValues();
+        $config = $this->config('rokka.settings');
 
-      $config->set('is_enabled', $values['is_enabled']);
-      $config->set('rokka_api_key', $values['rokka_api_key']);
-      $config->set('organization_name', $values['organization_name']);
-      $config->save();
+        $config->set('is_enabled', $values['is_enabled']);
+        $config->set('api_key', $values['api_key']);
+        $config->set('api_endpoint', $values['api_endpoint']);
+        $config->set('organization_name', $values['organization_name']);
+        $config->save();
 
-      parent::submitForm($form, $form_state);
-  }
+        parent::submitForm($form, $form_state);
+    }
 }
--- src/RokkaService.php
+++ src/RokkaService.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\rokka;
 
+use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\rokka\RokkaAdapter\SourceImageMetadata;
+use Psr\Log\LoggerInterface;
 use Rokka\Client\Base;
 use Rokka\Client\Factory;
 
@@ -12,6 +14,17 @@ use Rokka\Client\Factory;
  */
 class RokkaService implements RokkaServiceInterface
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var ConfigFactory
+     */
+    private $configFactory;
+
+
     private $apiKey;
     private $organizationName;
     private $apiEndpoint;
@@ -25,16 +38,25 @@ class RokkaService implements RokkaServiceInterface
      * RokkaService constructor.
      *
      * @param EntityTypeManagerInterface $em
-     * @param string                     $apiKey
-     * @param string                     $organizationName
-     * @param string                     $apiEndpoint
+     * @param LoggerInterface            $logger
+     *
+     * @internal param string $apiKey
+     * @internal param string $organizationName
+     * @internal param string $apiEndpoint
      */
-    public function __construct(EntityTypeManagerInterface $em, $apiKey = '', $organizationName = '', $apiEndpoint = null)
+    public function __construct(EntityTypeManagerInterface $em, ConfigFactory $configFactory, LoggerInterface $logger)
     {
-        $this->apiKey = $apiKey;
-        $this->organizationName = $organizationName;
-        $this->apiEndpoint = $apiEndpoint ?: Base::DEFAULT_API_BASE_URL;
         $this->entityManager = $em;
+        $this->logger = $logger;
+        $this->configFactory = $configFactory;
+
+        $config = $configFactory->get('rokka.settings');
+
+        $this->apiKey = $config->get('rokka_api_key');
+        $this->organizationName = $config->get('organization_name');
+        $this->apiEndpoint = $config->get('api_endpoint') ?: Base::DEFAULT_API_BASE_URL;
+
+        $logger->critical($this->apiEndpoint);
     }
 
     /**
@@ -91,5 +113,22 @@ class RokkaService implements RokkaServiceInterface
             ->range(null, 2);
         $metas = $q->execute();
         */
+    }
+
+    /**
+     * Return the given setting from the Rokka module configuration.
+     *
+     * Examples:
+     * - source_image_style (default: , 'rokka_source')
+     * - use_hash_as_name (default: true)
+     *
+     * @param string $param
+     *
+     * @return mixed
+     */
+    public function getSettings($param)
+    {
+        // TODO: Implement getSettings() method.
+        return false;
     }
 }
