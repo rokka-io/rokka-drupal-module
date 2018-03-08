35b2634298f4cf447845fc072224ceb23f0c74fd
Jonathan Minder <jonathan.minder@liip.ch>
2018-03-08 15:55:35 +0100

    Added Gaussian Blur filter implementation as new effect.

--- src/StyleEffects/EffectImageEffectsSetCanvas.php
+++ src/StyleEffects/EffectImageEffectsSetCanvas.php
@@ -16,10 +16,17 @@ class EffectImageEffectsSetCanvas implements InterfaceEffectImage {
   public static function buildRokkaStackOperation($data) {
     $options = [
       'mode' => 'foreground',
-      'width' => ImageStyleHelper::operationNormalizeSize($data['exact']['width']),
-      'height' => ImageStyleHelper::operationNormalizeSize($data['exact']['height']),
       'secondary_color' => substr(str_replace('#', '', $data['canvas_color']), 0, 6),
     ];
+
+    // width and height are optional.
+    if(!empty($data['exact']['width'])) {
+      $options['width'] = ImageStyleHelper::operationNormalizeSize($data['exact']['width']);
+    }
+    if(!empty($data['exact']['height'])) {
+      $options['height'] = ImageStyleHelper::operationNormalizeSize($data['exact']['height']);
+    }
+
     return [new StackOperation('composition', $options)];
   }
 
